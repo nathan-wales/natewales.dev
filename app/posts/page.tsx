37839d611e7369fae3ee50c0b83d1d23fd37ffa8
Nathan Wales <nathanwales@pm.me>
2023-09-18 09:09:37 -0600

    dynamically render post previews

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,15 +4,17 @@ import { getPostPreview, getAllPostIds } from "./lib/posts"
 export default function Posts() {
 
   const postIds: Array<number> = getAllPostIds()
-  console.log(postIds)
-  const testPost = getPostPreview(postIds[0]);
+  const postPreviews = new Array;
+  postIds.forEach( postId => {
+    postPreviews.push(getPostPreview(postId));
+  })
 
   return (
     <main>
       <div className=" text-blue-700 font-thin italic">
         Posts will go here for sure
         <div className="flex gap-4">
-          <PostPreview {...testPost} />
+          {postPreviews.map((preview, i) => <PostPreview {...preview} key={i} />)}
         </div>
       </div>
     </main>
