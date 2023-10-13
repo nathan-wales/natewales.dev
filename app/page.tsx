fbedae3227dcafd944ad296776df64e50d2bcb35
Nathan Wales <nathanwales@pm.me>
2023-10-13 09:24:17 -0600

    gradient name

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@
 export default function Home() {
   return (
     <main>
-      <div className="text-pink-400 font-extrabold">
-        wassup
+      <h1 className="text-7xl font-semibold font-mono bg-gradient-to-r from-purple-900  to-orange-600 text-transparent bg-clip-text">
+        nate wales
+      </h1>
+      <div>
+        doing epic things
       </div>
     </main>
   )
