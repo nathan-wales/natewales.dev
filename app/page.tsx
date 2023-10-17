e6de366119dfb5252962f42e597bd7b22aa13639
Nathan Wales <nathanwales@pm.me>
2023-10-17 08:38:14 -0600

    new color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-7xl font-semibold font-mono bg-gradient-to-r from-purple-900  to-orange-600 text-transparent bg-clip-text">
-        nate wales
+      <h1 className="text-7xl font-bold bg-gradient-to-r from-orange-600 via-pink-600 to-purple-700 text-transparent bg-clip-text font-sans">
+        NATE WALES
       </h1>
-      <div>
-        doing epic things
+      <div className="text-gray-500 font-semibold font-sans text-xl">
+        DOING EPIC THINGS
       </div>
     </main>
   )
